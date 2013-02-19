bc9294510a09ee33eec6d7ed23a5647ac86741c7
Udeep Shakya <udeepshakya546@gmail.com>
2013-02-19 10:12:17 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieColl::Application.config.secret_token = '0c4f8063cf55fdb44379ca5e33e678e0131b42c951298f551309ea80895e251f7f0850d94d68c118a9c2e2feae72e9f502ed1287d3f3ddbfad4ed9b586009afe'
